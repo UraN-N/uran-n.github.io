cb56b673bba45b05ee95676422f1552b1929f4a4
uran-n <3495373759@qq.com>
2025-07-07 22:13:17 +0800

    Site updated: 2025-07-07 22:13:17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1751896843340";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1751897591262";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",

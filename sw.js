cc01429db4222275c5940608a27aec845eb2f8ed
UraN-N <3495373759@qq.com>
2026-03-10 01:50:35 +0800

    Site updated: 2026-03-10 01:50:35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1773078265669";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1773078634788";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
